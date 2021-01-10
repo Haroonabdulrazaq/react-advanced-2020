034d8952a71f546919383de10da84bffc07bc8f4
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-10 17:39:48 +0100

    Finshed with useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
+import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
 // import Final from './tutorial/1-useState/final/1-error-example';
 
 function App() {
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,35 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([])
+
+  const getUsers = async () => {
+    const response = await fetch(url)
+    const users = await response.json()
+    setUsers(users)
+  }  
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  return <>
+    <h3>Github Users</h3>
+    <ul className="users">
+    {users.map((user, index)=>{
+      const {id, avatar_url, html_url, login} = user
+      return <li key={id}>
+         <img src={avatar_url} alt={login}/>
+         <div>
+         <h4>{login}</h4>
+         <a href={html_url}></a>
+         </div>
+      </li>
+      
+    })}
+    </ul>
+  </>
+
 };
 
 export default UseEffectFetchData;
+     
