5598bc290d37ba9910701c50853e677b97f2c3fe
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-11 11:35:42 +0100

    generated my own id using useState

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -11,15 +11,21 @@ const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('')
   const [email, setEmail] = useState('')
   const [person, setPerson] = useState([])
+  const [id, setId] = useState(1)
 
 
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault()
-   
-    if(firstName && email){
-      const newPerson = {firstName, email}  
-      setPerson((person)=>{
-       return  [...person, newPerson]
+    
+    setId((id)=>{
+      return id + 1
+    })
+
+    if (firstName && email) {
+      const newPerson = { id, firstName, email }
+      
+      setPerson((person) => {
+        return [...person, newPerson]
       })
     }
     setFirstName('')
@@ -35,8 +41,8 @@ const ControlledInputs = () => {
             type="text"
             name="firstName"
             value={firstName}
-            onChange={(e)=> setFirstName(e.target.value)}
-            required  />
+            onChange={(e) => setFirstName(e.target.value)}
+            required />
         </div>
         <div className="form-control">
           <label htmlFor="email">Email</label>
@@ -45,25 +51,25 @@ const ControlledInputs = () => {
             type="email"
             name="email"
             value={email}
-            onChange={(e)=> setEmail(e.target.value)}
-            required  />
+            onChange={(e) => setEmail(e.target.value)}
+            required />
         </div>
         <button className="btn">Add Person</button>
       </form>
     </article>
-    <h2 style={{marginTop:"2rem"}}>All Users</h2>
-    <article> 
-      {person.map((person, index)=>{ 
-        const {firstName, email} = person
+    <h2 style={{ marginTop: "2rem" }}>All Users</h2>
+    <article>
+      {person.map((person, index) => {
+        const { id, firstName, email } = person
         return (
-          <div className="item">
+          <div className="item" key={id}>
             <h3>{firstName}</h3>
             <p>{email}</p>
-          </div> 
+          </div>
         )
       })}
     </article>
-  
+
   </>;
 };
 
