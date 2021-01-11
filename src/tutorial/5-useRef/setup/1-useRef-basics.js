978121f0084624771b6f4afb5f1340caeb48f604
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-11 13:28:40 +0100

    Create a todoList wth add ref and useState

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,32 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState,useEffect, useRef } from 'react';
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null)
+  const [data, setData] = useState([])
+
+ const handleSubmit =(e)=>{
+  e.preventDefault();
+  setData([...data, refContainer.current.value])
+  refContainer.current.value =''
+  }
+  return <>
+  <h2 style={{margin: "2rem"}}>TodList</h2>
+  <form className="form" onSubmit={handleSubmit}>
+    <input type="text" ref ={refContainer}/>
+    <button className="btn" type="submit">Submit</button>
+  </form>
+  <ol>
+    {data.map((data, index)=>(
+      <li className="item" key={index}>
+        <p>{index } { data}</p>
+      </li>
+    ))}
+  </ol>
+  </>;
 };
 
 export default UseRefBasics;
