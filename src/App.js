5967492f458df9d03f02cf4a66742a73285007dc
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-14 17:15:35 +0100

    Learn reduce

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Setup from './tutorial/6-useReducer/setup/index';
+import Setup from './tutorial/6-useReducer/setup';
 // import Final from './tutorial/1-useState/final/1-error-example';
 
 function App() {
   return (
-    <div className='container' >
+    <div className='container'>
      <Setup />
     </div>
   )
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,45 +1,92 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
-import { data } from '../../../data';
-// reducer function
-const reducer =(state, action)=>{
+// import { data } from '../../../data';
 
+
+const reducer=(state, action)=> {
+
+  switch(action.type){
+    case 'ADD_ITEM':
+    const newPeople = [...state.people, action.payload]
+    return {
+          ...state,
+          people: newPeople,
+          isModalOpen: true,
+          modalContent: "item added"}
+    case 'NO_VALUE':
+      return {
+        ...state,
+        isModalOpen: true,
+        modalContent: "No value added"}
+    case 'CLOSE_MODAL':
+      return {
+        ...state,
+        isModalOpen: false
+      }
+    default:
+      throw new Error('No matching action type')
+  }
+  
 }
-
-const defaultState ={
+const defaultState = {
   people: [],
   isModalOpen: false,
-  modalContent: "Hello world"
+  modalContent: ""
 }
-const Index = () => {
-  const [name, setName] = useState('')
-   const [ state, dispatch] = useReducer(reducer, defaultState)
 
-  const handleSubmit=(e)=>{
+const Index = () => {
+  let [item, setItem] = useState('')
+  
+  const [state, dispatch] = useReducer(reducer, defaultState)
+
+  const handleSubmit =(e)=>{
     e.preventDefault()
-    if(name){
-    
+    if(item){
+      const newItem = {id: new Date().getTime().toString(), item}
+      console.log(newItem)
+      dispatch({
+        type: "ADD_ITEM",
+        payload: newItem
+      })
+      setItem('')
     }else{
-      
+      dispatch({type: 'NO_VALUE'})
     }
   }
+  const handleChange =(e)=>{
+   setItem(e.target.value)
+  }
 
+  const closeModal =()=>{
+    dispatch({
+      type: "CLOSE_MODAL"
+    })
+  }
+ 
   return <>
-      {state.isModalOpen && <Modal modalContent={state.ModalContent}/>}
-        <form onSubmit={handleSubmit} className="form">
-          <div >
-            <input type="text" 
-            value={name}
-            onChange={(e)=> setName(e.target.value)}/>
-          </div>
-          <button type="submit">Add</button>
-        </form>
-        {state.people.map((person, index)=>{
-          return (<div>
-            <h4>{person.name}</h4>
-          </div>)
-        })}
-      </>;
+   {state.isModalOpen && <Modal modalContent= {state.modalContent}/>}
+ <form onSubmit={handleSubmit} className='form'>
+        <div>
+          <input
+            type='text'
+            name="item"
+            value={item}
+            onChange={(e)=>{handleChange(e)}}
+          />
+        </div>
+        <button type='submit'>Add </button>
+ </form>
+
+ <ul>
+  
+   {state.people.map((listItem, index)=>{
+     return <div key={index}>
+       <h4 className="item">{listItem.item}</h4>
+     </div>
+   })}
+ </ul>
+  
+  </>;
 };
 
 export default Index;
