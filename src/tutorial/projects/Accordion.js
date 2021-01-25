29c0a7a9c02ed72f65dffe783808bc5fdeb268c3
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-25 17:13:07 +0100

    Adding layout to accordion

--- src/tutorial/projects/Accordion.js
+++ src/tutorial/projects/Accordion.js
@@ -5,8 +5,10 @@ class Accordion extends Component {
   render() {
     return (
       <div className="flex-container">
-        <h3 className="faq">Accordion on the way</h3>
-        <div className="question-answer"></div>
+        <h2 className="faq">Frequently Asked Questions </h2>
+        <div className="question-answer">
+
+        </div>
       </div>
     )
   }
