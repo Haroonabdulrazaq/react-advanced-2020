fb270eb9312a60c1048566f7a9ae09421103b918
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-26 17:08:53 +0100

    Add useEffect and settimeout

--- src/tutorial/projects/Slider.js
+++ src/tutorial/projects/Slider.js
@@ -4,6 +4,8 @@ import './Slider.css'
 
 const Slider = (props) => {
   let [counter, setCounter] = useState(0)
+  // let [timer, setTimer] = useState(5000)
+
   const handlePrev=()=>{
     if(counter === 0){
       setCounter(3)
@@ -11,6 +13,15 @@ const Slider = (props) => {
     }
     setCounter(counter - 1)
   }
+
+  const handleNext=()=>{
+    if(counter === 3){
+      setCounter(0)
+      return 
+    }
+    setCounter(counter + 1)
+  }
+  
 
   useEffect(() => {
     setTimeout(()=>{
@@ -23,13 +34,7 @@ const Slider = (props) => {
   
   }, [counter])
 
-  const handleNext=()=>{
-    if(counter === 3){
-      setCounter(0)
-      return 
-    }
-    setCounter(counter + 1)
-  }
+
 
   return (
     <section>
@@ -44,7 +49,7 @@ const Slider = (props) => {
             <p className="role">{data[counter]["role"]}</p>
             <p className="review">{data[counter]["review"]}</p>
             <div className="quote">
-              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z"></path></svg>
+              <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z"></path></svg>
             </div>
           </div>   
           <span className="next"  onClick={handleNext}>
