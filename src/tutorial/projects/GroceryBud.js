2f78d8224b8b537febe9233e623876fb4aac8602
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-27 13:31:01 +0100

    Setting up grocery form

--- src/tutorial/projects/GroceryBud.js
+++ src/tutorial/projects/GroceryBud.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import {MdLocalGroceryStore} from 'react-icons/all'
+import React, {useState} from 'react';
+import './GroceryBud.css'
 
 
 const GroceryBud = () => {
+  let [item, setItem] = useState('')
+
+  const handleSubmit =()=>{
+    
+  }
+
+
+
   return (
-    <div>
-      <h2><MdLocalGroceryStore/></h2>
+    <div className="wrapper">
+      <h2 className="heading">Grocery Bud</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+        type="text"
+        name= "grocery"
+        value= {item}
+        onChange={()=> setItem(e.target.value)}
+        />
+        <button className="btn">Submit</button>
+      </form>
     </div>
   )
 }
