441b68ba4ce3e0b128fe74f8dba718079f599b05
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-27 13:19:08 +0100

    Starte Grocery Bud project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/projects/OurTours';
+import Setup from './tutorial/projects/GroceryBud';
 
 function App() {
   return (
---
+++ src/tutorial/projects/GroceryBud.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {MdLocalGroceryStore} from 'react-icons/all'
+
+
+const GroceryBud = () => {
+  return (
+    <div>
+      <h2><MdLocalGroceryStore/></h2>
+    </div>
+  )
+}
+
+export default GroceryBud;
