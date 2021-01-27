e14b38eee22fbae04cfb4e56bc6ffe4b63a704e3
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-01-27 14:13:16 +0100

    Finshed the layout and styling

--- src/tutorial/projects/GroceryBud.js
+++ src/tutorial/projects/GroceryBud.js
@@ -1,28 +1,53 @@
 import React, {useState} from 'react';
-import './GroceryBud.css'
-
+import './GroceryBud.css';
+import {ImBin} from "react-icons/im";
+import {BsPencil} from "react-icons/bs";
 
 const GroceryBud = () => {
   let [item, setItem] = useState('')
+  let [list, setList] = useState([])
 
-  const handleSubmit =()=>{
-    
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    let newItem ={
+                  id: new Date().getTime().toString(),
+                  item: item
+                }
+                console.log(newItem)
+    setList([ ...list,
+      newItem
+    ])
+    setItem('')
   }
-
-
 
   return (
     <div className="wrapper">
       <h2 className="heading">Grocery Bud</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-        type="text"
-        name= "grocery"
-        value= {item}
-        onChange={()=> setItem(e.target.value)}
-        />
-        <button className="btn">Submit</button>
-      </form>
+      <form className='form' onSubmit={handleSubmit}>
+            <input
+              type='text'
+              name='item'
+              value={item}
+              onChange={(e)=> setItem(e.target.value)}/>
+          <button >Submit</button>
+        </form>
+
+        <article>
+          <ul className="unordered-list">
+          {list.map((listItem)=>{
+            const {id, item} = listItem
+            return <li className="item" key={id}>
+              <p >{item}</p>
+
+              <button className="icon-button">
+                <BsPencil color="green" title="edit"/>
+                <ImBin title="delete" color="red"/>
+              </button>
+            </li>
+          })}
+          </ul>
+          
+        </article>
     </div>
   )
 }
