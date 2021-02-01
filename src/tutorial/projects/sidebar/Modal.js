cc070eac8cf837baac0b4a1026d81a76c13edf51
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-02-01 16:47:44 +0100

    Fixed positioning

---
+++ src/tutorial/projects/sidebar/Modal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Modal = () => {
+  return (
+    <div>
+      <div className="modal-wrapper">
+        <h2>Am a modal</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Modal;
