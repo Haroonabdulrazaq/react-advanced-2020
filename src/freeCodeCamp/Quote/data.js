1c4e035666f5f9e2e0eb88bd95f07fb4807dbe18
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-02-05 09:26:14 +0100

    Finshed setting up data for qoute

--- src/freeCodeCamp/Quote/data.js
+++ src/freeCodeCamp/Quote/data.js
@@ -3,5 +3,51 @@ export const data =[
     quote: "It’s your place in the world; it’s your life. Go on and do all you can with it, and make it the life you want to live.",
     author: "Mae Jemison",
     color: "#58111A"
+  },
+  {
+    quote: "Definiteness of purpose is the starting point of all achievement.",
+    author: "W. Clement Stone",
+    color: "#002D62"
+  },
+  {
+    quote: "When I stand before God at the end of my life, I would hope that I would not have a single bit of talent left and could say, I used everything you gave me.",
+    author: "Erma Bombeck",
+    color: "#7FFFD4"
+  },
+  {
+    quote: "You become what you believe.",
+    author: "Oprah Winfrey",
+    color: "#00FF40"
+  },
+  {
+    quote: "Eighty percent of success is showing up.", 
+    author: "Woody Allen",
+    color: "#EE82EE"
+  },
+  {
+    quote: "Either write something worth reading or do something worth writing.",  
+    author: "Benjamin Franklin",
+    color: "#00308F"
+  },
+  {
+    quote:"The person who says it cannot be done should not interrupt the person who is doing it.",
+    author: "Chinese Proverb",
+    color:"#ED2939"
+  },
+  {
+    quote:"Happiness is not something readymade. It comes from your own actions.",
+    author: "Dalai Lama",
+    color:"#FE28A2"
+  },
+  {
+    quote:"You can't get a result, if you are not consistent",
+    author: "Haroon Abdulrazaq",
+    color:"#6495ED"
+  },
+  {
+    quote: "I am not the type who practice a thousand kick once, but the type who practice a kick a thousand times",
+    author: "Bruce lee",
+    color: "#00FA9A"
   }
+
 ]
